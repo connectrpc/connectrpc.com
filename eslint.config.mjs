a5af90bc974fe7f57520d1d375003345f46b924b
Steve Ayers <sayers@buf.build>
2024-10-01 14:27:50 -0400

    Upgrade to eslint v9
    Signed-off-by: Steve Ayers <sayers@buf.build>

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+// Copyright 2022-2023 The Connect Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["build/**/*"],
+}, ...compat.extends("plugin:@docusaurus/recommended"), {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+    },
+
+    rules: {
+        "@docusaurus/no-untranslated-text": ["warn", {
+            ignoredStrings: ["·", "—", "×"],
+        }],
+    },
+}];
